f9745204e4a802f2a2ba5652239f6f60c255d329
athoo <athoo@sina.com>
2015-09-02 15:21:24 +0800

    to mac

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-$(document).ready(console.log("hadskas"));
+$(document).ready(main());
+var str;
+
+function read_csv(path, file){
+  var path_to_file = path+"/"+file;
+
+  d3.csv( path_to_file ,function(error,csvdata){
+
+    if(error){
+      console.log(error);
+    }else{
+      str = d3.csv.format(csvdata);
+      // for (var i=0; i<csvdata.length; i++){
+      //   var state = csvdata[i].State;
+      //   var population = csvdata[i].Population;
+      //   var gdp = csvdata[i].GDP;
+      //   console.log(state+" "+population+" "+gdp);
+      // }
+    }
+
+  });
+}
+
+
+
+function main(){
+  read_csv("data", "table.csv");
+
+}
