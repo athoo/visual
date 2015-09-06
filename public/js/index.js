44902b8664aee551597ba02fdc03037d9c971580
athoo <athoo@sina.com>
2015-09-06 22:16:00 +0800

    add bar

--- public/js/index.js
+++ public/js/index.js
@@ -24,7 +24,8 @@ $(document).ready(function(){
 
 function main(){
   toggle_menu();
-  draw_mono_pie();
+  draw_bar_chart(Obj);
+  // draw_mono_pie();
 }
 
 var toggle_menu = function(){
@@ -33,6 +34,93 @@ var toggle_menu = function(){
       $("#wrapper").toggleClass("toggled");
   });
 };
+
+var Obj = {
+  'title': 'World\'s largest cities per 2014',
+  'subtitle': 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>',
+  'ytext': 'Population (millions)',
+  'tooltip': 'Population in 2008: <b>{point.y:.1f} millions</b>',
+  'name': 'Population',
+  'data':  [
+      ['Shanghai', 23.7],
+      ['Lagos', 16.1],
+      ['Instanbul', 14.2],
+      ['Karachi', 14.0],
+      ['Mumbai', 12.5],
+      ['Moscow', 12.1],
+      ['São Paulo', 11.8],
+      ['Beijing', 11.7],
+      ['Guangzhou', 11.1],
+      ['Delhi', 11.1],
+      ['Shenzhen', 10.5],
+      ['Seoul', 10.4],
+      ['Jakarta', 10.0],
+      ['Kinshasa', 9.3],
+      ['Tianjin', 9.3],
+      ['Tokyo', 9.0],
+      ['Cairo', 8.9],
+      ['Dhaka', 8.9],
+      ['Mexico City', 8.9],
+      ['Lima', 8.9]
+    ]
+}
+
+
+var draw_bar_chart = function (Obj) {
+
+
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: Obj.title
+        },
+        subtitle: {
+            text: Obj.subtitle
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: Obj.ytext
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: Obj.tooltip
+        },
+        series: [{
+            name: Obj.name,
+            data: Obj.data,
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+}
+
+
 
 
 var draw_mono_pie = function(){
@@ -44,20 +132,6 @@ var draw_mono_pie = function(){
     mono_pie_paintbrush(Obj);
   })
 
-  // var Obj = {
-  //   title: "This is a pie chart to visualization",
-  //   name: "play with browser",
-  //   data:[
-  //       {name: "Microsoft Internet Explorer", y: 56.33},
-  //       {name: "Chrome", y: 24.03},
-  //       {name: "Firefox", y: 10.38},
-  //       {name: "Safari", y: 4.77},
-  //       {name: "Opera", y: 0.91},
-  //       {name: "Proprietary or Undetectable", y: 0.2}
-  //   ]
-  // };
-
-  // mono_pie_paintbrush(Obj);
 };
 
 var mono_pie_paintbrush = function (Obj) {
